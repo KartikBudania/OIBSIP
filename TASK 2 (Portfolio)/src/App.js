86adc14216a9de288535117bd0aab12486150584
Kartik Budania <kartik.budania.02@gmail.com>
2023-09-29 14:55:32 +0530

    Portfolio Completed

---
+++ TASK 2 (Portfolio)/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import Projects from "./Projects";
+import About from "./About";
+import Tools from "./Tools";
+import ContactMe from "./ContactMe";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Projects />
+      <About />
+      <Tools />
+      <ContactMe />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ TASK 2 (Portfolio)/src/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-heading">Hello, I'm Kartik 👋</div>
+      <div className="hero-banner">
+        Front-End Web Developer<br></br>& a Boxing fan 👊🥊.
+        {/* <br></br>
+        die hard DragonBall Z fan, &<br></br>a Basketball player 🏀. */}
+      </div>
+      <div className="scroll-down"></div>
+    </section>
+  );
+};
+
+export default Hero;
